0621946e84b53294e8766d273e77d451f35dd963
Hao Ye <lhopitalified@gmail.com>
2019-02-17 16:11:49 -0500

    add arfima forecasting method and general forecasting wrapper

--- R/forecasting-arima.R
+++ R/forecasting-arima.R
@@ -1,29 +1,47 @@
-## original code
-# autoarima <- arfima(y.train) 
-# f = forecast(autoarima,NAHEAD)
-# predicted[which(dat$ID==w)[(n.y-NAHEAD+1):n.y],model.count] = f$mean
-# predictedSE[which(dat$ID==w)[(n.y-NAHEAD+1):n.y],model.count] = (f$upper[,2] - f$mean)/1.96 
-# model.output[[w,model.count]] = autoarima
-# model.names[model.count] = "ARIMA - with frac diff (arfima)"  
-
-autoarima <- function(dataset, num_ahead = 5)
+#' @title Make forward forecasts using a fractionally-differences ARIMA
+#' 
+#' @description This function performs the forecasting according to the 
+#'   approach in Ward et al. 2014, where the end of the time series is trimmed, 
+#'   and a forecasting model is fit to the remainder of the data. This model is 
+#'   then used to make 1-step ahead forecasts, projecting the forecasts forward 
+#'   for the end of the time series. (i.e. the first forecast is fed into the 
+#'   model to make the next forecast, and so forth).)
+#' 
+#' @param ts a single time series
+#' @param num_ahead the number of points at the end of the time series to forecast
+#' 
+#' @return a data.frame of the mean forecasts, the observed values, and the 
+#'   lower and upper 95 CI levels (if an error occurs, then just NA values)
+#' 
+#' @export
+#' 
+autoarima_ts <- function(ts, num_ahead = 5)
 {
-    num_points <- NROW(dataset$abundance)
-    training_subset <- seq_len(num_points - num_ahead)
-    
-    # for each time series in dataset
-    ts <- dplyr::pull(dataset$abundance, 1)
-    
-    
-    y.train <- ts[training_subset]
-    arima_model <- forecast::arfima(y.train) 
-    
-    f <- forecast::forecast(arima_model, num_ahead, level = 95)
-    # 
-    
-    f$mean
-    (f$upper - f$mean)/1.96
-    
+    tryCatch(
+        {
+            # trim NAs off of front and back
+            idx <- is.finite(ts)
+            ts_bounds <- range(which(idx))
+            ts <- ts[seq(ts_bounds[1], ts_bounds[2])]
+            
+            # set up model
+            num_points <- length(ts)
+            training_subset <- seq_len(num_points - num_ahead)
+            arima_model <- forecast::arfima(ts[training_subset]) 
+            
+            # make forecasts
+            f <- forecast::forecast(arima_model, num_ahead, level = 95)
+            
+            # return
+            data.frame(observed = as.numeric(tail(ts, num_ahead)), 
+                       predicted = as.numeric(f$mean), 
+                       lower_95 = as.numeric(f$lower), 
+                       upper_95 = as.numeric(f$upper))
+        }, error = function(e) {
+            warning(e, "  returning a NA object")
+            return(data.frame(observed = NA, 
+                              predicted = NA, 
+                              lower_95 = NA, 
+                              upper_95 = NA))
+        })
 }
-
-
---
+++ R/forecasting-wrapper.R
@@ -0,0 +1,62 @@
+#' @title Apply a forecasting function to all time series in a dataset
+#' 
+#' @description This wrapper applies a forecasting function to all the time 
+#'   series in a dataset, and then coerces the results and metadata into a 
+#'   combined object.
+#' 
+#' @param dataset the dataset object (following the `MATSS` data format)
+#' @param fun the forecasting function
+#' @param ... extra params to be passed to the forecasting function
+#' 
+#' @return a tibble with two list-columns:
+#'   \tabular{ll}{
+#'     \code{results} \tab a combined results data.frame\cr
+#'     \code{metadata} \tab the metadata component of the original dataset\cr
+#'   }
+#'   The results have all the columns returned from the forecasting function, 
+#'     with additional columns for the variable that was forecast (pulled from 
+#'     the original dataset), and a column for the forecast method
+#' 
+#' @examples
+#' \dontrun{
+#'   salmon <- readd(data_.salmon., cache = cache)
+#'   xx <- forecast_wrapper(salmon, autoarima_ts)
+#' }
+#' 
+#' @export
+#' 
+forecast_wrapper <- function(dataset, fun, ...)
+{
+    # Get the name of the dataset and the forecast method
+    method_name <- all.vars(match.call()$fun)
+    database_name <- all.vars(match.call()$dataset)
+    
+    # Get the variable names
+    var_names <- colnames(dataset$abundance)
+    
+    # Make the forecasts
+    forecasts <- purrr::map(dataset$abundance, fun, ...)
+    
+    # Assemble the formatted output into a single tibble, inserting in the 
+    #   variable names, the dataset name, and the forecast method
+    results <- 
+        pmap(list(forecasts = forecasts, 
+                  var_names = var_names, 
+                  method = method_name), 
+             function(forecasts, var_names, method) {
+                 mutate(forecasts, 
+                        variable = var_names, 
+                        method = method)
+             }) %>%
+        bind_rows() %>%
+        list()
+    names(results) <- database_name
+    
+    # Extract the metadata from the original dataset
+    metadata <- list(dataset$metadata)
+    names(metadata) <- database_name
+    
+    # Return the combined results and metadata
+    tibble(results = results, 
+           metadata = metadata)
+}
