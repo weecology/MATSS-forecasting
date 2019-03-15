54e68f664fe724cdc1b7f7840de50b4d02bf8b97
Hao Ye <lhopitalified@gmail.com>
2019-03-14 23:46:30 -0400

    update test to match new wrapper function

--- tests/testthat/test-10-wrapper-functions.R
+++ tests/testthat/test-10-wrapper-functions.R
@@ -11,17 +11,18 @@ test_that("forecast_wrapper works for simple functions", {
     
     # check if it worked properly and had the right size
     expect_error(output <- forecast_wrapper(data, fun), NA)
-    expect_equal(dim(output), c(1, 2))
+    expect_equal(dim(output), c(1, 4))
     
     # check results data.frame
     expect_error(results <- output$results[[1]], NA)
-    expect_equal(dim(results), c(11, 5))
+    expect_equal(dim(results), c(11, 4))
     expect_equal(results$id, names(data$abundance))
-    expect_true(all(results$method == "fun"))
+    expect_true(output$dataset == "data")
+    expect_true(output$method == "fun")
     
     # check metadata
     expect_identical(output$metadata[[1]], data$metadata)
     
     # check digest
-    expect_known_hash(digest::digest(output), "940980012f")
+    expect_known_hash(digest::digest(output), "2eb0517caa")
 })
