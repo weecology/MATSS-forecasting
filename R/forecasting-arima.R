7b2262903f8474ee523fd8a359aff7c4eef706ed
Hao Ye <lhopitalified@gmail.com>
2019-04-12 17:32:41 -0400

    remove dependencies to build Travis cache

--- R/forecasting-arima.R
+++ R/forecasting-arima.R
@@ -1,91 +1,91 @@
-#' @name arima_ts
-#' @title Make forecasts using AR / ARIMA
-#' @aliases randomwalk_ts, arima_fracdiff_ts
+#' #' @name arima_ts
+#' #' @title Make forecasts using AR / ARIMA
+#' #' @aliases randomwalk_ts, arima_fracdiff_ts
+#' #' 
+#' #' @rdname arima_ts
+#' #' 
+#' #' @description `arima_ts` fits an arima model
+#' #' 
+#' #' @inheritParams stats::arima
+#' #' @inheritParams make_forecasts
+#' #' @inheritParams forecast::forecast
+#' #' 
+#' #' @return a data.frame of the mean forecasts, the observed values, and the 
+#' #'   lower and upper CI levels (if an error occurs, then just NA values)
+#' #' 
+#' #' @importFrom stats ts
+#' #' @export
+#' #' 
+#' arima_ts <- function(timeseries, num_ahead = 5, level = 95, 
+#'                      order = c(1, 0, 0))
+#' {
+#'     f <- function(training, observed, level, order)
+#'     {
+#'         # make forecasts
+#'         arima_model <- stats::arima(ts(training), order = order)
+#'         
+#'         forecasts <- forecast::forecast(arima_model, NROW(observed), level = level)
+#'         
+#'         # return
+#'         data.frame(observed = as.numeric(observed),
+#'                    predicted = as.numeric(forecasts$mean),
+#'                    lower_CI = as.numeric(forecasts$lower),
+#'                    upper_CI = as.numeric(forecasts$upper))
+#'     }
+#'     
+#'     make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
+#'                    level = level, order = order)
+#' }
 #' 
-#' @rdname arima_ts
+#' #' @rdname arima_ts
+#' #' 
+#' #' @description `arima_fracdiff_ts` fits a step-wise fractionally differenced 
+#' #'   auto-arima 
+#' #' 
+#' #' @export
+#' #' 
+#' arima_fracdiff_ts <- function(timeseries, num_ahead = 5, level = 95)
+#' {
+#'     f <- function(training, observed, level)
+#'     {
+#'         # make forecasts
+#'         arima_model <- forecast::arfima(training) 
+#'         forecasts <- forecast::forecast(arima_model, NROW(observed), level = level)
 #' 
-#' @description `arima_ts` fits an arima model
+#'         # return
+#'         data.frame(observed = as.numeric(observed),
+#'                    predicted = as.numeric(forecasts$mean),
+#'                    lower_CI = as.numeric(forecasts$lower),
+#'                    upper_CI = as.numeric(forecasts$upper))
+#'     }
+#'     
+#'     make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
+#'                      level = level)
+#' }
 #' 
-#' @inheritParams stats::arima
-#' @inheritParams make_forecasts
-#' @inheritParams forecast::forecast
-#' 
-#' @return a data.frame of the mean forecasts, the observed values, and the 
-#'   lower and upper CI levels (if an error occurs, then just NA values)
-#' 
-#' @importFrom stats ts
-#' @export
-#' 
-arima_ts <- function(timeseries, num_ahead = 5, level = 95, 
-                     order = c(1, 0, 0))
-{
-    f <- function(training, observed, level, order)
-    {
-        # make forecasts
-        arima_model <- stats::arima(ts(training), order = order)
-        
-        forecasts <- forecast::forecast(arima_model, NROW(observed), level = level)
-        
-        # return
-        data.frame(observed = as.numeric(observed),
-                   predicted = as.numeric(forecasts$mean),
-                   lower_CI = as.numeric(forecasts$lower),
-                   upper_CI = as.numeric(forecasts$upper))
-    }
-    
-    make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
-                   level = level, order = order)
-}
-
-#' @rdname arima_ts
-#' 
-#' @description `arima_fracdiff_ts` fits a step-wise fractionally differenced 
-#'   auto-arima 
-#' 
-#' @export
-#' 
-arima_fracdiff_ts <- function(timeseries, num_ahead = 5, level = 95)
-{
-    f <- function(training, observed, level)
-    {
-        # make forecasts
-        arima_model <- forecast::arfima(training) 
-        forecasts <- forecast::forecast(arima_model, NROW(observed), level = level)
-
-        # return
-        data.frame(observed = as.numeric(observed),
-                   predicted = as.numeric(forecasts$mean),
-                   lower_CI = as.numeric(forecasts$lower),
-                   upper_CI = as.numeric(forecasts$upper))
-    }
-    
-    make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
-                     level = level)
-}
-
-#' @rdname arima_ts
-#' 
-#' @description `randomwalk_ts` fits a random walk model
-#' 
-#' @inheritParams forecast::rwf
-#' 
-#' @export
-#' 
-randomwalk_ts <- function(timeseries, num_ahead = 5, level = 95, 
-                          drift = FALSE)
-{
-    f <- function(training, observed, level, drift)
-    {
-        # make forecasts
-        forecasts <- forecast::rwf(training, NROW(observed), drift = drift, level = level)
-        
-        # return
-        data.frame(observed = as.numeric(observed),
-                   predicted = as.numeric(forecasts$mean),
-                   lower_CI = as.numeric(forecasts$lower),
-                   upper_CI = as.numeric(forecasts$upper))
-    }
-    
-    make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
-                   level = level, drift = drift)
-}
+#' #' @rdname arima_ts
+#' #' 
+#' #' @description `randomwalk_ts` fits a random walk model
+#' #' 
+#' #' @inheritParams forecast::rwf
+#' #' 
+#' #' @export
+#' #' 
+#' randomwalk_ts <- function(timeseries, num_ahead = 5, level = 95, 
+#'                           drift = FALSE)
+#' {
+#'     f <- function(training, observed, level, drift)
+#'     {
+#'         # make forecasts
+#'         forecasts <- forecast::rwf(training, NROW(observed), drift = drift, level = level)
+#'         
+#'         # return
+#'         data.frame(observed = as.numeric(observed),
+#'                    predicted = as.numeric(forecasts$mean),
+#'                    lower_CI = as.numeric(forecasts$lower),
+#'                    upper_CI = as.numeric(forecasts$upper))
+#'     }
+#'     
+#'     make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
+#'                    level = level, drift = drift)
+#' }
--- R/forecasting-ets.R
+++ R/forecasting-ets.R
@@ -1,64 +1,64 @@
-#' @name ets_ts
-#' @aliases sts_ts
-#' @title Make forecasts using an exponentially smoothed or structural time 
-#'   series mdoel
+#' #' @name ets_ts
+#' #' @aliases sts_ts
+#' #' @title Make forecasts using an exponentially smoothed or structural time 
+#' #'   series mdoel
+#' #' 
+#' #' @description Fit a time series model and make forecasts. The frequency of the 
+#' #'   data is set a priori, as opposed to estimating the parameter from the data. 
+#' #'   
+#' #'   \code{ets_ts} fits an exponentially smoothed time series model.
+#' #' 
+#' #' @inheritParams stats::ts
+#' #' @inheritParams make_forecasts
+#' #' @inheritParams forecast::forecast
+#' #' 
+#' #' @return a data.frame of the mean forecasts, the observed values, and the 
+#' #'   lower and upper CI levels (if an error occurs, then just NA values)
+#' #' 
+#' #' @export
+#' #' 
+#' ets_ts <- function(timeseries, num_ahead = 5, level = 95, frequency = 1)
+#' {
+#'     f <- function(training, observed, level, frequency)
+#'     {
+#'         # make forecasts
+#'         ts_model <- forecast::ets(ts(training, frequency = frequency))
+#'         forecasts <- forecast::forecast(ts_model, NROW(observed), level = level)
+#'         
+#'         # return
+#'         data.frame(observed = as.numeric(observed),
+#'                    predicted = as.numeric(forecasts$mean),
+#'                    lower_CI = as.numeric(forecasts$lower),
+#'                    upper_CI = as.numeric(forecasts$upper))
+#'     }
+#'     
+#'     make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
+#'                    level = level, frequency = frequency)
+#' }
 #' 
-#' @description Fit a time series model and make forecasts. The frequency of the 
-#'   data is set a priori, as opposed to estimating the parameter from the data. 
-#'   
-#'   \code{ets_ts} fits an exponentially smoothed time series model.
-#' 
-#' @inheritParams stats::ts
-#' @inheritParams make_forecasts
-#' @inheritParams forecast::forecast
-#' 
-#' @return a data.frame of the mean forecasts, the observed values, and the 
-#'   lower and upper CI levels (if an error occurs, then just NA values)
-#' 
-#' @export
-#' 
-ets_ts <- function(timeseries, num_ahead = 5, level = 95, frequency = 1)
-{
-    f <- function(training, observed, level, frequency)
-    {
-        # make forecasts
-        ts_model <- forecast::ets(ts(training, frequency = frequency))
-        forecasts <- forecast::forecast(ts_model, NROW(observed), level = level)
-        
-        # return
-        data.frame(observed = as.numeric(observed),
-                   predicted = as.numeric(forecasts$mean),
-                   lower_CI = as.numeric(forecasts$lower),
-                   upper_CI = as.numeric(forecasts$upper))
-    }
-    
-    make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
-                   level = level, frequency = frequency)
-}
-
-#' @name sts_ts
-#' @rdname ets_ts
-#' 
-#' @description \code{sts_ts} fits a structural model for a time series by 
-#'   maximum likelihood.
-#' 
-#' @export
-#' 
-sts_ts <- function(timeseries, num_ahead = 5, level = 95, frequency = 1)
-{
-    f <- function(training, observed, level, frequency)
-    {
-        # make forecasts
-        ts_model <- stats::StructTS(ts(training, frequency = frequency))
-        forecasts <- forecast::forecast(ts_model, NROW(observed), level = level)
-        
-        # return
-        data.frame(observed = as.numeric(observed),
-                   predicted = as.numeric(forecasts$mean),
-                   lower_CI = as.numeric(forecasts$lower),
-                   upper_CI = as.numeric(forecasts$upper))
-    }
-    
-    make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
-                   level = level, frequency = frequency)
-}
+#' #' @name sts_ts
+#' #' @rdname ets_ts
+#' #' 
+#' #' @description \code{sts_ts} fits a structural model for a time series by 
+#' #'   maximum likelihood.
+#' #' 
+#' #' @export
+#' #' 
+#' sts_ts <- function(timeseries, num_ahead = 5, level = 95, frequency = 1)
+#' {
+#'     f <- function(training, observed, level, frequency)
+#'     {
+#'         # make forecasts
+#'         ts_model <- stats::StructTS(ts(training, frequency = frequency))
+#'         forecasts <- forecast::forecast(ts_model, NROW(observed), level = level)
+#'         
+#'         # return
+#'         data.frame(observed = as.numeric(observed),
+#'                    predicted = as.numeric(forecasts$mean),
+#'                    lower_CI = as.numeric(forecasts$lower),
+#'                    upper_CI = as.numeric(forecasts$upper))
+#'     }
+#'     
+#'     make_forecasts(fun = f, timeseries = timeseries, num_ahead = num_ahead, 
+#'                    level = level, frequency = frequency)
+#' }
