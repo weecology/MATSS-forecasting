12c9e9ca198c99a2ef24c7c5ad7ab6546b90f00c
Hao Ye <lhopitalified@gmail.com>
2019-06-27 14:02:20 -0400

    fix tests

--- tests/testthat/test-21-edm.R
+++ tests/testthat/test-21-edm.R
@@ -15,27 +15,27 @@ test_that("smap_ts error checking works for too short time series", {
 test_that("simplex_ts function works", {
     expect_error(output <- simplex_ts(Nile), NA)
     expect_forecasts(output, c("observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "5459da4d0d")
+                     known_hash = "0744d46e1a")
 })
 
 test_that("simplex_ts function works for specified E", {
     expect_error(output <- simplex_ts(Nile, E_list = 3), NA)
     expect_forecasts(output, c("observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "e2a5cde309")
+                     known_hash = "c3e227e446")
 })
 
 test_that("smap_ts function works", {
     expect_error(output <- smap_ts(Nile), NA)
     expect_forecasts(round(output, 4), c("observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "5461ed855b")
+                     known_hash = "6df6c653d1")
 })
 
 test_that("smap_ts function works for specified E and theta", {
     expect_error(output <- smap_ts(Nile, E = 3), NA)
     expect_forecasts(round(output, 4), c("observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "54657b29ea")
+                     known_hash = "30e8ccd2e5")
     
     expect_error(output <- smap_ts(Nile, E = 3, theta = 1), NA)
     expect_forecasts(round(output, 4), c("observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "36bf95f751")
+                     known_hash = "60b217dce8")
 })
