dabd6a5e363dd5acb24b8418a60a7eb7ef29a749
Hao Ye <lhopitalified@gmail.com>
2019-08-02 15:36:28 -0400

    shorten tests for Travis

--- tests/testthat/test-20-marss.R
+++ tests/testthat/test-20-marss.R
@@ -19,15 +19,15 @@ test_that("marss_ts function works without drift", {
 })
 
 test_that("marss_one_step function works", {
-    expect_error(output <- marss_rw_one_step(Nile), NA)
+    expect_error(output <- marss_rw_one_step(Nile[1:30]), NA)
     expect_forecasts(output, 
                      c("time", "observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "256535fce9")
+                     known_hash = "f032bf174a")
 })
 
 test_that("marss_one_step function works without drift", {
-    expect_error(output <- marss_rw_one_step(Nile, drift = FALSE), NA)
+    expect_error(output <- marss_rw_one_step(Nile[1:30], drift = FALSE), NA)
     expect_forecasts(output, 
                      c("time", "observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "ce69cc8239")
+                     known_hash = "3187328fca")
 })
