cac3fb25557b88c4169cfcca40f685c0bb0fdc4c
Hao Ye <lhopitalified@gmail.com>
2019-08-03 00:15:07 -0400

    speed up tests

--- tests/testthat/test-20-marss.R
+++ tests/testthat/test-20-marss.R
@@ -7,27 +7,27 @@ test_that("marss_ts error checking works for too short time series", {
 })
 
 test_that("marss_ts function works", {
-    expect_error(output <- marss_ts(Nile), NA)
+    expect_error(output <- marss_ts(Nile[1:30]), NA)
     expect_forecasts(output, c("observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "9c3bc1db43")
+                     known_hash = "ac35ee4c82")
 })
 
 test_that("marss_ts function works without drift", {
-    expect_error(output <- marss_ts(Nile, drift = FALSE), NA)
+    expect_error(output <- marss_ts(Nile[1:30], drift = FALSE), NA)
     expect_forecasts(output, c("observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "d850380eae")
+                     known_hash = "ac50aa5f78")
 })
 
 test_that("marss_one_step function works", {
-    expect_error(output <- marss_rw_one_step(Nile[1:12]), NA)
+    expect_error(output <- marss_rw_one_step(Nile[1:9]), NA)
     expect_forecasts(output, 
                      c("time", "observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "c4c3d041d3")
+                     known_hash = "c8116f7add")
 })
 
 test_that("marss_one_step function works without drift", {
-    expect_error(output <- marss_rw_one_step(Nile[1:12], drift = FALSE), NA)
+    expect_error(output <- marss_rw_one_step(Nile[1:9], drift = FALSE), NA)
     expect_forecasts(output, 
                      c("time", "observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "28882cab16")
+                     known_hash = "de90c58976")
 })
