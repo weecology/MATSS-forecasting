366a26c9235d0fbad4d4eac7cb5a7e010a5cd7f5
Hao Ye <lhopitalified@gmail.com>
2019-08-05 00:17:02 -0400

    further shorten tests?

--- tests/testthat/test-21-marss-random-walk.R
+++ tests/testthat/test-21-marss-random-walk.R
@@ -1,15 +1,15 @@
 context("MARSS random walk functions")
 
 test_that("marss_one_step function works", {
-    expect_error(output <- marss_rw_one_step(Nile[1:9]), NA)
+    expect_error(output <- marss_rw_one_step(Nile[1:6]), NA)
     expect_forecasts(output, 
                      c("time", "observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "c8116f7add")
+                     known_hash = "2249d86991")
 })
 
 test_that("marss_one_step function works without drift", {
-    expect_error(output <- marss_rw_one_step(Nile[1:9], drift = FALSE), NA)
+    expect_error(output <- marss_rw_one_step(Nile[1:6], drift = FALSE), NA)
     expect_forecasts(output, 
                      c("time", "observed", "predicted", "lower_CI", "upper_CI"), 
-                     known_hash = "de90c58976")
+                     known_hash = "0755ab9ae0")
 })
