07a71e79b8640cecffc9e73161ae7928d94941dc
Hao Ye <lhopitalified@gmail.com>
2019-10-06 16:11:14 -0400

    add rest of PE functions and tests

--- R/pe_code.R
+++ R/pe_code.R
@@ -13,20 +13,20 @@
 #' @export
 check_time_series <- function(x)
 {
-  n <- length(x)
-  message("Many analysis methods assume equally spaced time series", 
-          ", please be careful!")
-  if (any(is.na(x)))
-    warning("Your time series contains some NAs. Be careful, as some analyses ", 
-            "/ functions deal well with NAs, and some don't.")
-  if (any(duplicated(x)))
-    warning("Your time series contain identical values, if there are many, ", 
-            "and if they appear consecutively, this could cause problems.")
-  if ( sum( (x[1:(n-1)] - x[2:n] ) == 0) > 0)
-    warning("Your time series has some identical consecutive values; ", 
-            "this could cause problems.")
-  ## check for long series of zeros (same values?)
-  invisible(NULL)
+    n <- length(x)
+    message("Many analysis methods assume equally spaced time series", 
+            ", please be careful!")
+    if (any(is.na(x)))
+        warning("Your time series contains some NAs. Be careful, as some analyses ", 
+                "/ functions deal well with NAs, and some don't.")
+    if (any(duplicated(x)))
+        warning("Your time series contain identical values, if there are many, ", 
+                "and if they appear consecutively, this could cause problems.")
+    if ( sum( (x[1:(n-1)] - x[2:n] ) == 0) > 0)
+        warning("Your time series has some identical consecutive values; ", 
+                "this could cause problems.")
+    ## check for long series of zeros (same values?)
+    invisible(NULL)
 }
 
 #' @title Embed a time series
@@ -45,27 +45,17 @@ check_time_series <- function(x)
 #' @export
 embedd <- function(x, m, d = 1, indices = FALSE, as.embed = FALSE)
 {
-  n <- length(x) - (m - 1)*d
-  X <- seq_along(x)
-  if (n <= 0)
-    stop("Insufficient observations for the requested embedding")
-  out <- matrix(rep(X[seq_len(n)], m), ncol = m)
-  out[,-1] <- out[,-1, drop = FALSE] + rep(seq_len(m - 1) * d, each = nrow(out))
-  if (as.embed)
-    out <- out[, rev(seq_len(ncol(out)))]
-  if (!indices)
-    out <- matrix(x[out], ncol = m)
-  out
-}
-
-#' @name Compute Shannon entropy
-#' @description Calculate the entropy (in bits) for a vector of word frequencies
-#' 
-#' @param wd the distribution of word frequencies
-#' @return a numeric value
-entropy <- function(wd)
-{
-  -sum(wd * log2(wd))
+    n <- length(x) - (m - 1)*d
+    X <- seq_along(x)
+    if (n <= 0)
+        stop("Insufficient observations for the requested embedding")
+    out <- matrix(rep(X[seq_len(n)], m), ncol = m)
+    out[,-1] <- out[,-1, drop = FALSE] + rep(seq_len(m - 1) * d, each = nrow(out))
+    if (as.embed)
+        out <- out[, rev(seq_len(ncol(out)))]
+    if (!indices)
+        out <- matrix(x[out], ncol = m)
+    out
 }
 
 #' #' Weighted distribution of "word"s in a time series.
@@ -84,136 +74,77 @@ entropy <- function(wd)
 #' #'  @return The word distribution.
 #' 
 
-#' Compute the distribution of "word"s in a time series.
+#' @title Compute the distribution of "word"s in a time series.
+#' @description `weighted_word_distribution` is a wrapper that sets `weighted` 
+#'   to `TRUE`
 #' @aliases weighted_word_distribution
 #'
 #' @param x_emb A time series of real numbers.
-#' @inheritParams rank
-#' @weighted whether to weight by the variance in each word
+#' @param weighted whether to weight by the variance in each word
+#' @inheritParams base::rank
 #' @return a table of the word distribution
+#' 
 #' @export
-
 word_distribution <- function(x_emb, ties.method = "first", weighted = TRUE)
 {
-  words <- apply(x_emb, 1, 
-                 function(x) paste(rev(rank(x, ties.method = ties.method)), collapse = "-"))
-  if (weighted)
-  {
-    weights <- apply(x_emb, 1, function(x) var(x))
-    return(aggregate(weights, list(words), sum)$x / sum(weights))
-  } else {
-    return(table(words)/nrow(x_emb))
-  }
+    words <- apply(x_emb, 1, 
+                   function(x) paste(rev(rank(x, ties.method = ties.method)), collapse = "-"))
+    if (weighted)
+    {
+        weights <- apply(x_emb, 1, function(x) var(x))
+        return(aggregate(weights, list(words), sum)$x / sum(weights))
+    } else {
+        return(as.numeric(table(words)/nrow(x_emb)))
+    }
 }
 
 #' @rdname word_distribution
 weighted_word_distribution <- function(x_emb, ties.method)
 {
-  word_distribution(x_emb = x_emb, ties.method = ties.method, weighted = TRUE)
+    word_distribution(x_emb = x_emb, ties.method = ties.method, weighted = TRUE)
 }
 
-
-
-#' Calculate the permuation entropy of a times series
-#'
-#' @param wd The distribution of counts (e.g., of words).
-#' @return The entropy of the distribution.
+#' @title Calculate the permuation entropy of a times series
+#' @aliases WPE
+#' @description `WPE` is a wrapper that sets `weighted` to `TRUE`
 #' @details Words containing NAs are ignored.
+#'
+#' @inheritParams embedd
+#' @inheritParams word_distribution
+#' @return The entropy of the distribution.
+#' 
 #' @examples
 #' x <- rnorm(1000)
-#' PE(x, TRUE, 3, 1, "first")
+#' PE(x, 3)
 #' @export
-PE <- function(x, weighted, word_length, tau, tie_method, noise_amount=NA) {
-  
-  ## check if the time series contains all very very similar values
-  if(log10(var(x, na.rm=TRUE)) < -20) {
-    warning("Time series has variance < 1e-20, so setting entropy to 0.")
-    ent <- 0
-  }
-  
-  if(log10(var(x, na.rm=TRUE)) >= -20) {
-    ## Add noise if that method used to prevent ties
-    if(tie_method=="noise") {
-      tie_method <- "average" ## ties.method used by rank is irrelevant if one adds noise, as there will be no ties
-      x <- x + runif(length(x)) * 10^(-noise_amount - 1)
+PE <- function(x, m, d = 1, ties.method = "first", weighted = TRUE)
+{
+    ## check if the time series contains low variance
+    if (var(x, na.rm = TRUE) <= 2 * .Machine$double.eps)
+    {
+        warning("Time series variance is too small, so setting entropy to 0.")
+        return(0)
     }
     
-    ## Embed the data
-    x_emb <- embedd(x=x, m=word_length, d=tau)
-    
-    ## Remove words containing NAs
+    x_emb <- embedd(x = x, m = m, d = d)
     x_emb <- na.omit(x_emb)
+    wd <- word_distribution(x_emb, ties.method = ties.method, weight = weighted)
     
+    if (ties.method == "average")
+    {
+        # with tied ranks allowed, the number of possible words increases
+        denom <- log2(2 * factorial(m))
+    } else {
+        denom <- log2(factorial(m))
+    }
     
-    ## Calculate entropy
-    if(!weighted)
-      wd <- word_distribution(x_emb, tie_method)
-    if(weighted)
-      wd <- weighted_word_distribution(x_emb, tie_method)
-    
-    if(tie_method=="average")
-      denom <- log2(2*factorial(word_length))
-    if(tie_method!="average")
-      denom <- log2(factorial(word_length))
-    
-    
-    ent <- entropy(wd)/denom
-  }
-  ent
+    # Calculate the entropy (in bits) for a vector of word frequencies
+    entropy <- function(wd) {  -sum(wd * log2(wd))  }
+    entropy(wd) / denom
 }
 
-
-
-
-#' Entropy of a distribution of counts.
-#'
-#' @param wd The distribution of counts (e.g., of words).
-#' @return The entropy of the distribution.
-#' @details Words containing NAs are ignored.
-rolling_PE <- function(x, weighted, word_length, tau, tie_method, noise_amount=NA,
-                       windowsize=10){
-  
-  ## Add noise if that method used to prevent ties
-  if(tie_method=="noise") {
-    tie_method <- "average" ## ties.method used by rank is irrelevant if one adds noise, as there will be no ties
-    x <- x + runif(length(x)) * 10^(-noise_amount - 1)
-  }
-  
-  ## Embed the data
-  x_emb <- embedd(x=x, m=word_length, d=tau)
-  
-  words <- apply(x_emb, 1, function(x) paste(rev(rank(x, ties.method = tie_method, na.last="keep")), collapse="-"))
-  weights <- apply(x_emb, 1, function(x) var(x))
-  
-  
-  if(tie_method=="average")
-    denom <- log(2*factorial(word_length))
-  if(tie_method!="average")
-    denom <- log(factorial(word_length))
-  
-  
-  if(!weighted)
-    PE <- rollapply(words,
-                    width=windowsize,
-                    function(x) {
-                      ifelse(1-(sum(grepl("NA", x)))/length(x)>threshold_prop_valid_words_in_window,
-                             entropy(table(x[!grepl("NA", x)]))/denom,
-                             NA)
-                    },
-                    fill=NA)
-  
-  
-  
-  
-  if(weighted)
-    PE <- rollapply(data.frame(weights, words),
-                    width=windowsize,
-                    function(z) entropy(aggregate(as.numeric(z[,1]), list(z[,2]), sum)$x / sum(as.numeric(z[,1])))/denom,
-                    by.column=FALSE,
-                    fill=NA)
-  
-  
-  
-  PE
-  
+#' @rdname PE
+WPE <- function(x, m, d, ties.method = "first")
+{
+    PE(x, m, d, ties.method, weighted = TRUE)
 }
--- tests/testthat/test-05-permutation-entropy.R
+++ tests/testthat/test-05-permutation-entropy.R
@@ -3,8 +3,35 @@ context("Check Permutation Entropy Functions")
 test_that("embedd works", {
     ts <- as.numeric(1:10)
     expect_equal(embedd(ts, 3), matrix(c(1:8, 2:9, 3:10), ncol = 3))
-  
+    expect_equal(embedd(ts, 1), matrix(1:10, ncol = 1))
+    expect_error(embedd(ts, 11), "Insufficient observations for the requested embedding")
     
+    expect_equal(embedd(ts, 3, as.embed = TRUE), matrix(c(3:10, 2:9, 1:8), ncol = 3))
+    expect_equal(embedd(ts, 3, 3), matrix(c(1:4, 4:7, 7:10), ncol = 3))
     
-      
+    ts <- rnorm(10)
+    expect_equal(embedd(ts, 3, indices = TRUE), matrix(c(1:8, 2:9, 3:10), ncol = 3))
+})
+
+test_that("word_distribution works", {
+    ts <- as.numeric(Nile)
+    x_emb <- embedd(ts, 3)
+    expect_equal(word_distribution(x_emb, weighted = FALSE), 
+                 c(17, 18, 14, 16, 20, 13) / 98)
+    expect_equal(round(word_distribution(x_emb), 4), 
+                 c(0.2129, 0.1580, 0.1006, 0.1514, 0.2047, 0.1724))
+})
+
+test_that("permutation entropy calculations work", {
+    ts <- as.numeric(Nile)
+    expect_equal(round(PE(ts, 2), 4), 0.9995)
+    expect_equal(round(PE(ts, 3), 4), 0.9853)
+    expect_equal(round(PE(ts, 4), 4), 0.9394)
+
+    expect_equal(round(PE(ts, 2, weighted = FALSE), 4), 0.9993)
+    expect_equal(round(PE(ts, 3, weighted = FALSE), 4), 0.9942)
+    expect_equal(round(PE(ts, 4, weighted = FALSE), 4), 0.9684)
+    
+    expect_equal(round(PE(ts, 4, ties.method = "last"), 4), 0.9362)
+    expect_equal(round(PE(ts, 4, ties.method = "average"), 4), 0.8023)
 })
